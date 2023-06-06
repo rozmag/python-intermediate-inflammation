b35d4ed03890c66d576a967258cf8dc07c59cfcc
Roza Maghsood <roza.maghsood@astrazeneca.com>
2023-06-06 09:50:39 +0200

    Docstring improvements.

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -27,7 +27,7 @@ def main(args):
 
         views.visualize(view_data)
 
-        
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='A basic patient inflammation data management system')
